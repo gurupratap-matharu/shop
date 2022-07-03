4f644000af2977d9987a1021fcb9fdd9ccaaa8f4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-07-03 16:53:36 -0600

    Implement ProductList and ProductDetail View

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from typing import Optional
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+
+from .models import Category, Product
+
+
+class ProductList(ListView):
+    model = Product
+    context_object_name: Optional[str] = 'product_list'
+    paginate_by: int = 9
+    template_name: str = 'shop/product_list.html'
+
+
+class ProductDetail(DetailView):
+    model = Product
+    context_object_name: Optional[str] = 'product'
+    template_name: str = 'shop/product_detail.html'
