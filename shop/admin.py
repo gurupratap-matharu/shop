247e19088cc5036c96304c84a1bde2331c25b059
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-07-03 15:30:14 -0600

    Add Product and Catalog to django admin interface

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
+from typing import Sequence
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields: dict[str, Sequence[str]] = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display: list[str] = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter: list[str] = ['available', 'created', 'updated']
+    list_editable: list[str] = ['price', 'available']
+    prepopulated_fields: dict[str, Sequence[str]] = {'slug': ('name',)}
