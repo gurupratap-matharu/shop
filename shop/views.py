30df2fae505e52014d6cd7ec4172ec6ce8dead9d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-07-04 16:23:26 -0600

    Filter queryset with available products

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
 from .models import Category, Product
@@ -8,12 +8,28 @@ from .models import Category, Product
 
 class ProductList(ListView):
     model = Product
-    context_object_name: Optional[str] = 'product_list'
+    context_object_name: Optional[str] = 'products'
     paginate_by: int = 9
     template_name: str = 'shop/product_list.html'
+
+    def get_queryset(self):
+        products = Product.objects.filter(available=True)
+        categories = Category.objects.all()
+        category_slug = self.kwargs.get('category_slug')
+
+        if category_slug:
+            category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=category)
+            self.extra_context = {'category': category, 'categories': categories}
+
+        return products
 
 
 class ProductDetail(DetailView):
     model = Product
     context_object_name: Optional[str] = 'product'
     template_name: str = 'shop/product_detail.html'
+
+    def get_object(self, queryset=None):
+        product = get_object_or_404(Product, id=self.kwargs['id'], slug=self.kwargs['category_slug'], available=True)
+        return product
