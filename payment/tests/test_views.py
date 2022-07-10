0740711cb0d3bd136d55aabfbbf2942363992a2c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-07-10 15:23:16 -0600

    Add placeholder tests

--- payment/tests/test_views.py
+++ payment/tests/test_views.py
@@ -9,6 +9,12 @@ def test_payment_process_page_works(client):
 
 
 @pytest.mark.django_db
+def test_payment_process_uses_correct_html_template(client):
+    response = client.get(reverse("payment:process"))
+    assert False
+
+
+@pytest.mark.django_db
 def test_payment_done_page_works(client):
     response = client.get(reverse("payment:done"))
     assert response.status_code == 200
