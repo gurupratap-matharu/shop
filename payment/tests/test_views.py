0454c4b490c4097da1222ac95743da589825dcfc
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-07-10 11:37:41 -0600

    format using black

---
+++ payment/tests/test_views.py
@@ -0,0 +1,20 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_payment_process_page_works(client):
+    response = client.get(reverse("payment:process"))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_payment_done_page_works(client):
+    response = client.get(reverse("payment:done"))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_payment_canceled_page_works(client):
+    response = client.get(reverse("payment:canceled"))
+    assert response.status_code == 200
