bce813ec21ba115d68147a910ec6a5a721f6167b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-07-10 11:29:21 -0600

    Rectify view for Payment Cancellation

--- payment/urls.py
+++ payment/urls.py
@@ -7,5 +7,5 @@ app_name = "payment"
 urlpatterns = [
     path("process/", views.payment_process, name="process"),
     path("done/", views.payment_done, name="done"),
-    path("canceled/", views.payment_process, name="canceled"),
+    path("canceled/", views.payment_canceled, name="canceled"),
 ]
