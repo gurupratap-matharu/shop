da6d2626e35ed120171417de87183fac9babae36
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-07-31 21:16:38 +0100

    Wire up admin_order_detail view

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,9 @@ app_name = "orders"
 
 urlpatterns = [
     path("create/", views.OrderCreateView.as_view(), name="order_create"),
+    path(
+        "admin/order/<int:order_id>/",
+        views.admin_order_detail,
+        name="admin_order_detail",
+    ),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,15 @@
 import logging
 from typing import Any, Dict
 
-from django.shortcuts import redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.generic.edit import FormView
 
 from cart.cart import Cart
 
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import Order, OrderItem
 from .tasks import send_order_creation_mail
 
 logger = logging.getLogger(__name__)
@@ -20,7 +21,7 @@ class OrderCreateView(FormView):
 
     def form_valid(self, form):
         logger.info("Order form is valid :)")
-        order = form.save()
+        order = form.save()  # type: ignore
         cart = Cart(self.request)
 
         # Think of putting this Orderline creation as a method in the Order model. View should not handle it
@@ -38,7 +39,7 @@ class OrderCreateView(FormView):
         send_order_creation_mail.delay(order.id)
 
         # Set the order_id in the session
-        self.request.session["order_id"] = order.id # TODO WHY DO WE DO THIS?
+        self.request.session["order_id"] = order.id  # TODO WHY DO WE DO THIS?
 
         # Redirect for payment
         return redirect(reverse("payment:process"))
@@ -47,3 +48,14 @@ class OrderCreateView(FormView):
         context = super().get_context_data(**kwargs)
         context["cart"] = Cart(self.request)
         return context
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    """
+    Custom django admin view to see the detail of any order. This view is meant to be
+    used by staff members.
+    """
+
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "admin/orders/order_detail.html", {"order": order})
