89f72b7f541b79b37784f47f699002720940c877
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-08-01 17:16:08 +0100

    Add LocalMiddleware and Languages to settings

--- main/settings.py
+++ main/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -115,7 +116,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en"
+LANGUAGES = (
+    ("en", "English"),
+    ("es", "Spanish"),
+)
 
 TIME_ZONE = "UTC"
 
