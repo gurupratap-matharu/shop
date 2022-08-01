90eedcd1b253747427553ee588ec98054c3cc26c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-08-01 12:06:58 +0100

    Set static_root settings

--- main/settings.py
+++ main/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
