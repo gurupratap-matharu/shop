59a91ee26105fc66e408acbcaac3c551bbed6b73
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-08-02 19:30:26 +0100

    Add i18n urls translations

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
 from . import views
 
 app_name = "orders"
 
 urlpatterns = [
-    path("create/", views.OrderCreateView.as_view(), name="order_create"),
+    path(_("create/"), views.OrderCreateView.as_view(), name="order_create"),
     path(
         "admin/order/<int:order_id>/",
         views.admin_order_detail,
