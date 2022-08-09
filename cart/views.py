b28146aa98824e6fb100442f64cfa162ed59c589
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-08-09 15:33:34 +0100

    Implement recommendation for products in cart

--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.http import require_POST
 
 from coupons.forms import CouponApplyForm
 from shop.models import Product
+from shop.recommender import Recommender
 
 from .cart import Cart
 from .forms import CartAddProductForm
@@ -19,10 +20,19 @@ def cart_detail(request):
         )
     coupon_apply_form = CouponApplyForm()
 
+    cart_products = [item["product"] for item in cart]
+
+    r = Recommender()
+    recommended_products = r.suggest_products_for(cart_products, max_results=4)
+
     return render(
-        request,
-        "cart/cart_detail.html",
-        {"cart": cart, "coupon_apply_form": coupon_apply_form},
+        request=request,
+        template_name="cart/cart_detail.html",
+        context={
+            "cart": cart,
+            "coupon_apply_form": coupon_apply_form,
+            "recommended_products": recommended_products,
+        },
     )
 
 
